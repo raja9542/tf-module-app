c54114f8835bebe33641f174c769536e387011a8
raja9542 <rajasekhar1banda@gmail.com>
2023-11-16 20:55:36 +0530

    rabbitmq endpoint creation and storing in parameter store

--- main.tf
+++ main.tf
@@ -47,7 +47,8 @@ resource "aws_iam_policy" "policy" {
           "arn:aws:ssm:us-east-1:994733300076:parameter/nexus*",
           "arn:aws:ssm:us-east-1:994733300076:parameter/${var.env}.docdb*",
           "arn:aws:ssm:us-east-1:994733300076:parameter/${var.env}.elasticache*",
-          "arn:aws:ssm:us-east-1:994733300076:parameter/${var.env}.rds*"
+          "arn:aws:ssm:us-east-1:994733300076:parameter/${var.env}.rds*",
+          "arn:aws:ssm:us-east-1:994733300076:parameter/${var.env}.rabbitmq*"
         ]
       },
       {
