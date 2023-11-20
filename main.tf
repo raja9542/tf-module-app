0d5d7ee33918fc4f45f149f2f4aad4301d2a1aea
raja9542 <rajasekhar1banda@gmail.com>
2023-11-20 19:36:14 +0530

    app module autoscaling policy added

--- main.tf
+++ main.tf
@@ -151,6 +151,18 @@ resource "aws_autoscaling_group" "asg" {
   }
 }
 
+resource "aws_autoscaling_policy" "cpu-tracking-policy" {
+  name        = "whenCPULoadIncrease"
+  policy_type = "TargetTrackingScaling"
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 30.0
+  }
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+}
+
 resource "aws_route53_record" "app" {
   zone_id = "Z09171912J6RDBH9U9MN3"
   name    = "${var.component}-${var.env}.devopsraja66.online"
